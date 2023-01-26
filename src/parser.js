98ec6b2fca1aacaf5b67a6575f2685b94c7f66a4
Vojta Lančarič <vojta.lancaric@email.cz>
2023-01-26 18:40:10 +0100

    Funkcni zmeny mistnosti

--- src/parser.js
+++ src/parser.js
@@ -1,18 +1,8 @@
 //var input = document.getElementById("prompt");
 
-function makeListOf(map){
-    return Array.from(map.keys());
-    /*
-    var keys = [];
-    for(const key in map){
-        keys.push(key);
-    }
-    return keys;*/
-}
+function makeListOf(map){ return Array.from(map.keys()); }
 
-document.addEventListener("keydown", (event) => {
-    keyPressed(event); 
-});
+document.addEventListener("keydown", (event) => { keyPressed(event); });
 
 //vypise text do div hra
 function println(text, delay = 0){
@@ -48,10 +38,6 @@ class Parser{
         return text;
     }
 
-    score_word(obraz, vzor){
-        var score = 0;
-    }
-
     //vybereme nejvhodnejsi slova
     mostApropriate(sep, set_of_words){
         var command = "";
@@ -66,40 +52,53 @@ class Parser{
     }
 
 
+    //funkce spočítá, jak moc si odpovídají slova
+    fits(word1, word2){
+        var score = 0;
+        //word1 je vstup, word2 je slovo ze hry
+        var l = word1.length;
+        
+        for(var i = 0; i < l - 2; i++){
+            if(word2.includes(word1.substring(i, i + 4)) == true){
+                return 0;
+            }
+        }
+        return Number.POSITIVE_INFINITY;
+    }
+
 
     //najdeme nejlepsi vyraz 
     handleCommand(sep, listOfPoss){
         
         var l = sep.length;
         var max_score = 0;
+        var minScore =  Number.POSITIVE_INFINITY;//nejaka hodnota pro infinity
+        var minInfo;
 
-/*        for(var poss in listOfPoss){
-            
-            var splt = poss.split();
-            var score = 0;
-            for(var i = 0; i < l; i++){ //pro vsechny pocatecni pozice 
-                
-                for(
-                score +=  
-            }    
-        }*/
-
-        //vyber vhodny prikaz 
-        //var command = mostApropriate(command, listOfPoss);
-        //zatim budeme uvazovat jednoslovne prikazy
-
-        //zatim uvazujeme jen jednoslovne prikazy
-        var length = 0;
-        var command = "";
-
-        if(sep.length != 0){
-            command = sep[0];
-            length = 1;
+        for(var j = 0; j < listOfPoss.length; j++){
+            var sp = listOfPoss[j].split(" "); //splitneme prikaz mezerami
+            for(var i = 0; i < sep.length - sp.length + 1; i++){
+                var val = this.fits(sep[i], sp[0]); //testujeme jenom prvni slovo
+                if(val < minScore){
+                    minScore = val;
+                    minInfo = {
+                        i: i, //zacatek commandu
+                        j: j, //index commandu
+                        l: sp.length, //delka prikazu
+                    } 
+                }
+            }
         }
+        
+        //nejaka konstanta 
+        if(minInfo === undefined || minScore > 1000){
+            return new ReturnObject(-1, "", sep); //nepovedlo se nic najit
+        }
+        
+        //odstrani vsechna slova, vcetne provedeneho prikazu
+        sep = sep.slice(minInfo.i + minInfo.l);
 
-        sep = sep.slice(length);
-        //zmensi command list o 
-        return new ReturnObject(length, command, sep);
+        return new ReturnObject(minInfo.l, listOfPoss[minInfo.j], sep);
     }
 }
 
@@ -117,6 +116,11 @@ class Objects{
     }
     describe(sep){
         println(this.description);
+    }
+
+    //je zavolano, pokud dany objekt nema nejakou metodu
+    errorMessage(sep, _this, command){
+        println("Na " + _this.name + "nemůžeš zavolat tento příkaz");
     }
 
     //navratova hodnota, jestli se zvednuti podarilo - treba jestli neni moc tezka vec
@@ -138,20 +142,22 @@ class Room{
     constructor(name, description = ""){
         this.objMap = new Map(); //mapa objektů
         this.fnMap = new Map();  //mapa funkcí
+        this.fnMap.set("moveTo", this.moveTo); //zakladni funkce 
         this.description = description;
         this.name = name;
-        //this.nbrs = nbrs;
+        this.nbrs = []; //seznam sousedu
         
         this.fnMap.set("describe", this.describe);
 
         //funkce, ktere volame na objekty
     }
     
-    moveTo(sep, _this){
-        var command = sep[0];
-        if(neighbours.includes(command)){
-            
+    moveTo(sep,command, _this){
+
+        if(_this.nbrs.includes(command)){
+            return 1;        
         }
+        return 0; //neni v seznamu sousedu, nelze se presunout  
     }
     
     describe(sep,_this){
@@ -171,11 +177,10 @@ class Game{
         this.room = "velin";
        
         this.roomMap = new Map();
-
         this.fnMap = new Map();
 
         this.fnMap.set("jdi", this.moveTo);
-        this.fnMap.set("popis", this.describe);
+        this.fnMap.set("rozhledni se", this.describe);
         this.fnMap.set("pomoc", this.help );
         this.fnMap.set("poloz", this.drop);
         this.fnMap.set("zvedni", this.pickUp);
@@ -183,12 +188,16 @@ class Game{
     }
 
     //prikazy budou ve forme - sloveso, slova mezi, (mistnost, predmet, vec);
-    inputCommand(text){
-        console.log(this);
-        if(text == "") return;
-        var command = parser.removePunctuation(text);
-        var sep = command.split(" ");
-        //najdi nejlepsi prikaz
+    inputCommand(command){
+        if(command == "") return;
+
+        //uprav prikaz
+        command = parser.removePunctuation(command);
+        command = command.replace(/\s\s+/g, ' '); //odstrani mezery, nahradi za jednu
+        var sep = command.split(" "); 
+        sep = sep.filter((str) => (str) != ""); //odstranime prazdne stringy
+
+        //najdi nejlepsi funkci
         var retObj = parser.handleCommand(sep, makeListOf(this.fnMap));
         sep = retObj.sep; //novy, zmenseny seznam prikazu 
 
@@ -196,19 +205,38 @@ class Game{
             println("Zadal jsi neznámý příkaz. Pro vypsání nápovědy zadej příkaz 'pomoc'.");
             return;
         }
-        console.log(retObj.command);
+
         //zavolej funkci se zmensenym listem parametru 
         (this.fnMap.get(retObj.command))(sep, this);
-        //if(this.fnMap[retObj.command](sep)) println("command se zdařil"); 
     }
 
     moveTo(sep, _this){
-        console.log(_this);
         var toRoom = parser.handleCommand(sep, makeListOf(_this.roomMap));
         
-        if(toRoom.length == 0){
+        if(toRoom.length == 0 && sep.length != 0){
             println("Do takové místnosti nemůžeš jít."); 
+            return false;
         } 
+        //zkontroluj, ze se muzeme presunout do dane mistnosti
+        var room = _this.roomMap.get(_this.room);
+        console.log(room.name);
+
+        //zachytit navratovy kod
+        //presun je mozny
+        var state = room.fnMap.get("moveTo")(sep, toRoom.command, room);
+
+        if(toRoom.command == _this.room){
+            println("Jsi ve " + _this.room); 
+            return; 
+        }
+        if(state == 1){
+            _this.room = toRoom.command;     
+            println("Jdeš do " + _this.room);
+        }
+        else{
+            println("To je moc daleko"); 
+        }
+        console.log(toRoom.command);
     }
 
     //rovnou presmerovat na room
@@ -288,28 +316,32 @@ var parser = new Parser();
 window.addEventListener("load", game.intro);
 
 function init(){
+    //mistnosti 
+    game.roomMap.set('velin',new Room("velin", "Nacháziš se ve velíně raketoplánu. Na palubní desce před tebou červeně blikají nejrůznější přepínače a kontrolky. Alarm poškození levého křídla se rozhoukal vysokým tónem."));
+    game.roomMap.set('nakladovy prostor',new Room('nakladovy prostor', "Kolem vidíš sklad nejrůznějšího nářadí. Bohudík je upevněné a nepoletuje si tu jen tak. Po bližším průzkumu se ti uleví. Technici NASA počítali skoro se vším. Na pravé straně se nachází magnetická stěna, na které jsou připevněny veškeré nástroje, které by se mohly hodit - aku vrtačka, plazmová svářečka, dokonce i náhradní keramické destičky."));
+    game.roomMap.set('prechodova komora',new Room('prechodova komora', "Místnost tvaru koule o poloměru asi 1,5 metru. Kolem sebe vidíš tlačítka na otevření dveří vedoucí do velína a do volného vesmíru."));
 
-    game.roomMap.set('velin',new Room("velin", "nejaky popis super husty velina"));
-    game.roomMap.set('nakladovy_prostor',new Room('nakladovy_prostor', "nejaky popis nakladoveho prostoru"));
-   
-    var naklad = game.roomMap.get('nakladovy_prostor');
-    naklad.objMap.set("sroubovak", new Objects("sroubovak", "popis sroubovaku")); 
+    //velin
+    game.roomMap.get('velin').objMap.set("vysilacka", new Objects("vysilacka", "Dlouhovnná vysílačka, která slouží ke komunikaci s odletovým střediskem."));
+    game.roomMap.get('velin').objMap.set("nabijecka", new Objects("vysilacka", "Nabíječka na vysílačku s univerzálním NASA konektorem."));
+    game.roomMap.get('velin').objMap.set("ovládací panel", new Objects("ovládací panel", "Při bližším pohledu na hlavní kontrolní panel na něm vidíš tlačítka "));
+    game.roomMap.get('velin').nbrs = ["nakladovy prostor"];
+    
 
-    game.roomMap.get('velin').objMap.set('jablko', new Objects("jablko", "nejaky husty popis jablka"));
-
-
-    //this.roomMap['velin'].description = " Nějaký super hustý popis velína."; 
-    //this.roomMap['dolni pauba'] = " Dalši popis paluby";
-    //(game.roomMap.get('velin')).objMap.set("jablko", new Objects("jablko", "Maličkaté kulaťoučké červeňoučké jablíčko.")); 
-    //(game.roomMap['velin']).objMap.set("jablko", new Objects("jablko", "Maličkaté kulaťoučké červeňoučké jablíčko.")); 
-    //game.roomMap['velin'].objMap.set("hruška", new Objects("hruška", "Maličkaté kulaťoučké červeňoučké hruška.")); 
-    //game.roomMap['velin'].objMap.set("ovladač", new Objects("ovladač", "Maličkaté kulaťoučké červeňoučké hruška.")); 
-
-    //game.roomMap['velin'].fnMap.set
+    
+    //nakladovy prostor
+    game.roomMap.get('nakladovy prostor').objMap.set("aku vrtacka", new Objects("aku vrtacka", "Vratčka, kterou jde použít jako elektrický šroubovák. Je poháněná elektřinou z baterie.")); //neni nabita
+    game.roomMap.get('nakladovy prostor').objMap.set("pilnik", new Objects("pilnik", "Jemný pilník na železo."));
+    game.roomMap.get('nakladovy prostor').objMap.set("sverak", new Objects("sverak", "Jednoduchý svěrák na železo, do kterého jde něco upnout."));
+    game.roomMap.get('nakladovy prostor').objMap.set("keramicke desticky", new Objects("keramicke desticky", "Destičky slouží jako vnější ochrana raketoplánu proti vysoké teplotě při přistání."));
+    game.roomMap.get('nakladovy prostor').nbrs = ["velin"];
+    //game.roomMap.get('nakladovy prostor').objMap.set("skafandr", new Objects("skafandr", "Vesmírný skafandr, sloužící k volného prostoru."));
+    
+    //prechodova komora
+    game.roomMap.get('prechodova komora').objMap.set("skafandr", new Objects("skafandr", "Vesmírný skafandr, sloužící k pohybu ve volném prostoru."));
 }
 
 function keyPressed(e){
-
     if(e.keyCode == 13){
         var input = document.getElementById('input');
         game.inputCommand(input.value);
